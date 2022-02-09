4f3045e9dad85fbeef8db68d417b503f12557e68
Ben8120 <chuabenshern5@gmail.com>
2022-02-09 17:57:34 +0800

    Hello world!

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Purpose of this application is to test and learn the usage of React for final year project :)
         </p>
         <a
           className="App-link"
