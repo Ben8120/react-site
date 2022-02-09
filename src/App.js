b330ec6f7524a19165a622cfd9aa264c6e476597
Ben8120 <chuabenshern5@gmail.com>
2022-02-10 00:09:45 +0800

    removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Purpose of this application is to test and learn the usage of React for final year project :)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello World!</p>
     </div>
   );
 }
