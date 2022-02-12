d7bf4753744393739faea69bb6a45de7a8ff0d63
Ben8120 <chuabenshern5@gmail.com>
2022-02-12 21:05:33 +0800

    added base css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import Navbar from "./components/Navbar";
 
-import Home from "./screens/Home";
-
 function App() {
-  const title = "Welcome to Ben's React Blogs!";
-  const likes = 50;
-  const person = { name: 'Ben', age: 22 }; 
-  const link = "https://www.google.com";
-
   return (
     <div>
     <Navbar />
-      <Home />
+      {/* {<Home />} */}
 
-      <a href={ link }>Google Site</a>
     </div>
   );
 }
