8e1ed89a75dadcc78004356c1f2968b553429006
Ben8120 <chuabenshern5@gmail.com>
2022-02-14 10:50:31 +0800

    catching errors in js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,6 +5,7 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     //useEffect runs in every render
     //[] enpty dependency makesit only run on first load
@@ -12,11 +13,19 @@ const Home = () => {
       setTimeout(() => {
         fetch('http://localhost:8000/blogs')
           .then(res => {
+            if (!res.ok) {
+              throw Error('could not fetch data for that resource')
+            }
             return res.json();
           })
           .then(data => {
             console.log(data);
             setBlogs(data);
+            setIsPending(false);
+            setError(false);
+          })
+          .catch(err => {
+            setError(err.message);
             setIsPending(false);
           })
       }, 1000)
@@ -24,6 +33,7 @@ const Home = () => {
     
     return ( 
         <div className="home">
+          { error && <div>{error}</div>}
           {isPending && <div>loading...</div>}
           {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
